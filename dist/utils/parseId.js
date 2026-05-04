ec17903a1cee286b59cd6fb4d43ce7a0f7fb66c0
Yash-Soni-07 <yashsoni0701@gmail.com>
2026-05-04 21:19:40 +0530

    chore: fix .gitignore and remove generated/secret files from tracking

--- dist/utils/parseId.js
+++
@@ -1,9 +0,0 @@
-export const parseId = (idParam) => {
-    if (!idParam || Array.isArray(idParam))
-        return null;
-    const id = Number(idParam);
-    if (!id || isNaN(id))
-        return null;
-    return id;
-};
-//# sourceMappingURL=parseId.js.map
